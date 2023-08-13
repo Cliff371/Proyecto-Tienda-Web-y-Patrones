c018fd055cbed46fe215f0336bcb7e48c510d71d
Dell <Dell@DESKTOP-OOGJE1I>
2023-08-13 15:39:34 -0600

    Semana 13

---
+++ src/main/java/Tienda/demo/dao/FacturaDao.java
@@ -0,0 +1,8 @@
+package Tienda.demo.dao;
+
+import Tienda.demo.domain.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaDao extends JpaRepository <Factura,Long> {
+     
+}
---
+++ src/main/java/Tienda/demo/dao/VentaDao.java
@@ -0,0 +1,9 @@
+package Tienda.demo.dao;
+
+import Tienda.demo.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaDao extends JpaRepository <Venta,Long> {
+     
+}
+
